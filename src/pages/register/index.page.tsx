52710e0fd6e7f774f0bf8581f2b9483fb8279b13
Willian Oliveira <willian.oliveira@itsector.pt>
2023-03-05 12:13:43 +0000

    Initial value in input user

--- src/pages/register/index.page.tsx
+++ src/pages/register/index.page.tsx
@@ -1,3 +1,6 @@
+import { useEffect } from 'react';
+
+import { useRouter } from 'next/router';
 import { ArrowRight } from 'phosphor-react';
 import { useForm } from 'react-hook-form';
 import { z } from 'zod';
@@ -24,10 +27,19 @@ export default function Register() {
   const {
     register,
     handleSubmit,
+    setValue,
     formState: { errors, isSubmitting }
   } = useForm<RegisterFormData>({
     resolver: zodResolver(registerFormSchema)
   });
+
+  const router = useRouter();
+
+  useEffect(() => {
+    if (router.query.username) {
+      setValue('username', String(router.query.username));
+    }
+  }, [router.query?.username, setValue]);
 
   function handleRegister(data: RegisterFormData) {
     console.log(data);
