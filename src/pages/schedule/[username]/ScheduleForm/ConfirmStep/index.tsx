9e797da587756205f0b13b26761ac1bb4f9663f1
Willian Oliveira <willian.oliveira@itsector.pt>
2023-03-07 19:35:46 +0000

    Block time and finish query

--- src/pages/schedule/[username]/ScheduleForm/ConfirmStep/index.tsx
+++ src/pages/schedule/[username]/ScheduleForm/ConfirmStep/index.tsx
@@ -1,3 +1,4 @@
+import dayjs from 'dayjs';
 import { CalendarBlank, Clock } from 'phosphor-react';
 import { useForm } from 'react-hook-form';
 import { z } from 'zod';
@@ -15,7 +16,12 @@ const confirmFormSchema = z.object({
 
 type ConfirmFormData = z.infer<typeof confirmFormSchema>;
 
-export function ConfirmStep() {
+interface ConfirmStepProps {
+  schedulingDate: Date;
+  onCancelConfirmation: () => void;
+}
+
+export function ConfirmStep({ schedulingDate, onCancelConfirmation }: ConfirmStepProps) {
   const {
     register,
     handleSubmit,
@@ -23,6 +29,9 @@ export function ConfirmStep() {
   } = useForm<ConfirmFormData>({
     resolver: zodResolver(confirmFormSchema)
   });
+
+  const describedDate = dayjs(schedulingDate).format('DD[ de ]MMMM[ de ]YYYY');
+  const describedTime = dayjs(schedulingDate).format('HH:mm[h]');
 
   function handleConfirmScheduling(data: ConfirmFormData) {
     console.log(data);
@@ -33,11 +42,11 @@ export function ConfirmStep() {
       <FormHeader>
         <Text>
           <CalendarBlank />
-          22 de Setembro de 2022
+          {describedDate}
         </Text>
         <Text>
           <Clock />
-          18:00h
+          {describedTime}
         </Text>
       </FormHeader>
 
@@ -59,7 +68,7 @@ export function ConfirmStep() {
       </label>
 
       <FormActions>
-        <Button type="button" variant="tertiary">
+        <Button type="button" variant="tertiary" onClick={onCancelConfirmation}>
           Cancelar
         </Button>
         <Button type="submit" disabled={isSubmitting}>
--- src/utils/get-week-days.ts
+++ src/utils/get-week-days.ts
@@ -5,7 +5,7 @@ interface GetWeekDaysParams {
 export function getWeekDays({ short = false }: GetWeekDaysParams = {}) {
   const formatter = new Intl.DateTimeFormat('pt-BR', { weekday: 'long' });
 
-  return Array.from(Array(7).keys())
+  const weekDays = Array.from(Array(7).keys())
     .map((day) => formatter.format(new Date(Date.UTC(2021, 5, day))))
     .map((weekDay) => {
       if (short) {
@@ -14,4 +14,15 @@ export function getWeekDays({ short = false }: GetWeekDaysParams = {}) {
 
       return weekDay.substring(0, 1).toUpperCase().concat(weekDay.substring(1));
     });
+
+  return arrayMove(weekDays, 6, 0);
+}
+
+function arrayMove(arr: any[], fromIndex: number, toIndex: number) {
+  const element = arr[fromIndex];
+
+  arr.splice(fromIndex, 1);
+  arr.splice(toIndex, 0, element);
+
+  return arr;
 }
