a043ae1d3f065cebbc48d194291f71c64cf724a2
Willian Oliveira <willian.oliveira@itsector.pt>
2023-03-07 20:13:57 +0000

    Google api integration

--- src/pages/schedule/[username]/ScheduleForm/ConfirmStep/index.tsx
+++ src/pages/schedule/[username]/ScheduleForm/ConfirmStep/index.tsx
@@ -1,4 +1,5 @@
 import dayjs from 'dayjs';
+import { useRouter } from 'next/router';
 import { CalendarBlank, Clock } from 'phosphor-react';
 import { useForm } from 'react-hook-form';
 import { z } from 'zod';
@@ -6,6 +7,7 @@ import { z } from 'zod';
 import { zodResolver } from '@hookform/resolvers/zod';
 import { Button, Text, TextArea, TextInput } from '@ignite-ui/react';
 
+import { api } from '../../../../../lib/axios';
 import { ConfirmForm, FormActions, FormError, FormHeader } from './styles';
 
 const confirmFormSchema = z.object({
@@ -33,8 +35,20 @@ export function ConfirmStep({ schedulingDate, onCancelConfirmation }: ConfirmSte
   const describedDate = dayjs(schedulingDate).format('DD[ de ]MMMM[ de ]YYYY');
   const describedTime = dayjs(schedulingDate).format('HH:mm[h]');
 
-  function handleConfirmScheduling(data: ConfirmFormData) {
-    console.log(data);
+  const router = useRouter();
+  const username = String(router.query.username);
+
+  async function handleConfirmScheduling(data: ConfirmFormData) {
+    const { name, email, observations } = data;
+
+    await api.post(`/users/${username}/schedule`, {
+      name,
+      email,
+      observations,
+      date: schedulingDate
+    });
+
+    onCancelConfirmation();
   }
 
   return (
